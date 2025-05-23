0a4e84f207d5d0457194799fc83046a68cb70db6
ronan <ronan_mccabe@protonmail.com>
2025-05-23 13:20:04 -0600

    v2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("launch:", Date.now());
+const { execSync } = require("child_process");
+const { resolve } = require("path");
+
+const projectRoot = resolve(__dirname, "..");
+const content = execSync(`npm run scan --silent`, {
+  encoding: "utf8",
+  cwd: projectRoot,
+});
+
+console.log("v" + content.trim());
